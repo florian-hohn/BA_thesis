24a679a621ff89b7412d3f6b8dd3a226db7fad7f
Florian, Hohn <florian.hohn@student.fhws.de>
2020-01-16 14:10:59 +0100

    created csvs and managed to succesfull load data

--- BA_thesis_code/BA_thesis_code/BAThesisCode.py
+++ BA_thesis_code/BA_thesis_code/BAThesisCode.py
@@ -1,21 +1,33 @@
 import numpy
-import skmultiflow
-from skmultiflow.data import WaveformGenerator
-from skmultiflow.trees import HoeffdingTree
-from skmultiflow.evaluation import EvaluatePrequential
+import pandas as pd
+from skmultiflow.prototype import robust_soft_learning_vector_quantization
+from skmultiflow.data.file_stream import FileStream
+from skmultiflow.data.data_stream import DataStream
 
-#to see if it works
-# 1. Create a stream
-stream = WaveformGenerator()
-stream.prepare_for_use()
+#Workflow from scikit_multiflow framework
+# 1. Load the data set as a stream
+dfn = [["realData/electric_data.csv", " "],
+       ["realData/outdoor.csv", " "],
+       ["realData/poker_data.csv", " "],
+       ["realData/weather_data.csv", ","]]
 
-# 2. Instantiate the HoeffdingTree classifier
-ht = HoeffdingTree()
+df = pd.read_csv(dfn[0][0],dfn[0][1])
+df = df.drop(df.columns[0], axis=1)
+
+dataStream = DataStream(df)
+dataStream.prepare_for_use()
+
+#1.1 retrieve the first sample 
+print("Data: ")
+print(dataStream.next_sample())
+
+# 2. load the classifier that you want to use
+#rslvqAdap = defaultRSLVQ()
 
 # 3. Setup the evaluator
-evaluator = EvaluatePrequential(show_plot=True,
-                                pretrain_size=200,
-                                max_samples=20000)
+#evaluator = EvaluatePrequential(show_plot=True,
+                               # pretrain_size=200,
+                               # max_samples=20000)
 
 # 4. Run evaluation
-evaluator.evaluate(stream=stream, model=ht)
+#evaluator.evaluate(stream=stream, model=ht)
