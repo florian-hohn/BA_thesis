91084c4cd357537ff26c1596958f6f91c84cc7c2
Florian, Hohn <florian.hohn@student.fhws.de>
2020-01-16 16:20:28 +0100

    some refactoring

--- BA_thesis_code/BA_thesis_code/Algorithms/RSLVQAdadelta.py
+++ BA_thesis_code/BA_thesis_code/Algorithms/RSLVQAdadelta.py
@@ -1,7 +1,7 @@
-import numpy
-import skmultiflow.prototype
+import numpy as np
+from skmultiflow.prototype.robust_soft_learning_vector_quantization import RobustSoftLearningVectorQuantization as RSLVQ
     
-class RSLVQAdadelta(skmultiflow.prototype.robust_soft_learning_vector_quantization):
+class RSLVQAdadelta(RSLVQ):
     """Inherits from the scikit-multiflow framework implementation 
     of the rslvq algortihm and modifies it to an implementation where the decition is done with the adadelta"""
 
--- BA_thesis_code/BA_thesis_code/BAThesisCode.py
+++ BA_thesis_code/BA_thesis_code/BAThesisCode.py
@@ -1,33 +1,35 @@
 import numpy
 import pandas as pd
 from skmultiflow.prototype.robust_soft_learning_vector_quantization import RobustSoftLearningVectorQuantization as RSLVQ
-from skmultiflow.data.file_stream import FileStream
+from skmultiflow.drift_detection.adwin import ADWIN
 from skmultiflow.data.data_stream import DataStream
 
 #Workflow from scikit_multiflow framework
 # 1. Load the data set as a stream
-dfn = [["realData/electric_data.csv", " "],
-       ["realData/outdoor.csv", " "],
-       ["realData/poker_data.csv", " "],
-       ["realData/weather_data.csv", ","]]
+dfn = [["realData/electric_data.csv", " "], # 7 attributes
+       ["realData/outdoor_data.csv", " "],  # 5 attributes
+       ["realData/poker_data.csv", " "],    # 10 attributes
+       ["realData/weather_data.csv", ","]]  # 8 attributes
 
 df = pd.read_csv(dfn[0][0],dfn[0][1])
 df = df.drop(df.columns[0], axis=1)
+
+
 
 dataStream = DataStream(df)
 dataStream.prepare_for_use()
 
 #1.1 retrieve the first sample 
 print("Data: ")
-print(dataStream.next_sample())
+print(dataStream.next_sample(5))
 
 # 2. load the classifier that you want to use
-#rslvqAdap = defaultRSLVQ()
+clf_base_rslvq = RSLVQ()
 
 # 3. Setup the evaluator
-#evaluator = EvaluatePrequential(show_plot=True,
-                               # pretrain_size=200,
-                               # max_samples=20000)
+evaluator_rslvq = EvaluatePrequential(show_plot=True,
+                                pretrain_size=200,
+                                max_samples=20000)
 
 # 4. Run evaluation
 #evaluator.evaluate(stream=stream, model=ht)
