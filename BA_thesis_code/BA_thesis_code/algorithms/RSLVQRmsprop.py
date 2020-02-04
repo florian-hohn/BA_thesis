220fddd4d7e66ed423df855fd9b8c071c812809a
Florian, Hohn <florian.hohn@student.fhws.de>
2020-02-04 15:11:49 +0100

    forgot something

--- BA_thesis_code/BA_thesis_code/algorithms/RSLVQRmsprop.py
+++ BA_thesis_code/BA_thesis_code/algorithms/RSLVQRmsprop.py
@@ -1,43 +1,27 @@
 import numpy
 from skmultiflow.prototype.robust_soft_learning_vector_quantization import RobustSoftLearningVectorQuantization as RSLVQ
 
-class RSLVQRmsprop(RSLVQ):
+class RSLVQRMSprop(object):
     """description of class"""
     def _optimize(self, x, y, random_state):
-        """Implementation of Adaptive RMSprop"""
-        n_data, n_dim = x.shape
+        """Implementation of RMSprop"""
+        n_data, n_dim = X.shape
         nb_prototypes = self.c_w_.size
         prototypes = self.w_.reshape(nb_prototypes, n_dim)
 
         for i in range(n_data):
-            xi = x[i]
+            xi = X[i]
             c_xi = y[i]
             for j in range(prototypes.shape[0]):
                 d = (xi - prototypes[j])
-
                 if self.c_w_[j] == c_xi:
                     gradient = (self._p(j, xi, prototypes=self.w_,
-                                y=c_xi) - self._p(j, xi, prototypes=self.w_))
+                    y=c_xi) - self._p(j, xi, prototypes=self.w_))* d
                 else:
-                    gradient = self._p(j, xi, prototypes=self.w_)
-
-                # calc adaptive decay rate
-                dec_rate = np.minimum(np.absolute(self._costf(j=j,
-                                                                x=gradient**2,
-                                                                w=self.squared_mean_gradient[j])), 0.9)
-
-                self.decay_rate[j] = 1.0 - dec_rate
+                    gradient = - self._p(j, xi, prototypes=self.w_)* d
 
                 # Accumulate gradient
-                self.squared_mean_gradient[j] = self.decay_rate[j] * self.squared_mean_gradient[j] + \
-                (1 - self.decay_rate[j]) * gradient ** 2
+                self.squared_mean_gradient[j] = 0.9 * self.squared_mean_gradient[j] + 0.1 * gradient ** 2
 
                 # Update Prototype
-                if self.c_w_[j] == c_xi:
-                    self.w_[j] += (self.learning_rate /
-                        ((self.squared_mean_gradient[j] + \
-                        self.epsilon) ** 0.5)) * gradient * d
-                else:
-                    self.w_[j] -= (self.learning_rate /
-                        ((self.squared_mean_gradient[j] + \
-                        self.epsilon) ** 0.5)) * gradient * d
+                self.w_[j] += (self.learning_rate / ((self.squared_mean_gradient[j] + self.epsilon) ** 0.5)) * gradient
