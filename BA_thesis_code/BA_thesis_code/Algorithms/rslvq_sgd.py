f3e7778bc1eaf674513b42eeff5aa3033b4eb734
Florian, Hohn <florian.hohn@student.fhws.de>
2020-02-05 15:49:12 +0100

    changed the sgd class

--- BA_thesis_code/BA_thesis_code/Algorithms/rslvq_sgd.py
+++ BA_thesis_code/BA_thesis_code/Algorithms/rslvq_sgd.py
@@ -1,9 +1,13 @@
+import numpy as np
+from sklearn.utils import validation
+from sklearn.metrics import euclidean_distances
+from sklearn.utils.multiclass import unique_labels
+from skmultiflow.core.base import ClassifierMixin, BaseSKMObject
 from .base_rslvq import BaseRSLVQ
 
 class RSLVQSgd(BaseRSLVQ):
     def __init__(self, prototypes_per_class=1, initial_prototypes=None,
-                 sigma=1.0, random_state=None, gradient_descent='sgd',
-                 gamma=0.9):
+                 sigma=1.0, random_state=None, gamma=0.9):
         self.sigma = sigma
         self.random_state = random_state
         self.epsilon = 1e-8
@@ -13,7 +17,6 @@ class RSLVQSgd(BaseRSLVQ):
         self.classes_ = []
         self.learning_rate = 1 / sigma
         self.gamma = gamma
-        self.gradient_descent = gradient_descent
 
         if sigma <= 0:
             raise ValueError('Sigma must be greater than 0')
@@ -22,5 +25,121 @@ class RSLVQSgd(BaseRSLVQ):
         if gamma >= 1 or gamma < 0:
             raise ValueError('Decay rate gamma has to be between 0 and\
                              less than 1')
+
+    def _update_prototype(self, j, xi, c_xi, prototypes):
+        """SGD"""
+        d = xi - prototypes[j]
+
+        if self.c_w_[j] == c_xi:
+            # Attract prototype to data point
+            self.w_[j] += self.learning_rate * \
+                (self._p(j, xi, prototypes=self.w_, y=c_xi) -
+                 self._p(j, xi, prototypes=self.w_)) * d
+        else:
+            # Distance prototype from data point
+            self.w_[j] -= self.learning_rate * self._p(
+                j, xi, prototypes=self.w_) * d
+
+    def _validate_train_parms(self, train_set, train_lab, classes=None):
+        random_state = validation.check_random_state(self.random_state)
+        train_set, train_lab = validation.check_X_y(train_set,
+                                                    train_lab.ravel())
+
+        if self.initial_fit:
+            if classes:
+                self.classes_ = np.asarray(classes)
+                self.protos_initialized = np.zeros(self.classes_.size)
+            else:
+                self.classes_ = unique_labels(train_lab)
+                self.protos_initialized = np.zeros(self.classes_.size)
+
+            # Validate that labels have correct format
+            for i in range(len(self.classes_)):
+                if i not in self.classes_:
+                    raise ValueError('Labels have to be ascending int,\
+                                     starting at 0, got {}'
+                                     .format(self.classes_))
+
+        nb_classes = len(self.classes_)
+        nb_features = train_set.shape[1]
+
+        # set prototypes per class
+        if isinstance(self.prototypes_per_class, int):
+            # ppc is int so we can give same number ppc to for all classes
+            if self.prototypes_per_class < 0:
+                raise ValueError('prototypes_per_class must be a positive int')
+            # nb_ppc = number of protos per class
+            nb_ppc = np.ones([nb_classes],
+                             dtype='int') * self.prototypes_per_class
+        elif isinstance(self.prototypes_per_class, list):
+            # its an array containing individual number of protos per class
+            # - not fully supported yet
+            nb_ppc = validation.column_or_1d(
+                validation.check_array(self.prototypes_per_class,
+                                       ensure_2d=False, dtype='int'))
+            if nb_ppc.min() <= 0:
+                raise ValueError(
+                    'values in prototypes_per_class must be positive')
+            if nb_ppc.size != nb_classes:
+                raise ValueError(
+                    'length of prototypes_per_class'
+                    ' does not fit the number of classes'
+                    'classes=%d'
+                    'length=%d' % (nb_classes, nb_ppc.size))
+        else:
+            raise ValueError('Invalid data type for prototypes_per_class, '
+                             'must be int or list of int')
+
+        # initialize prototypes
+        if self.initial_prototypes is None:
+            if self.initial_fit:
+                self.w_ = np.empty([np.sum(nb_ppc), nb_features],
+                                   dtype=np.double)
+                self.c_w_ = np.empty([nb_ppc.sum()], dtype=self.classes_.dtype)
+            pos = 0
+            for actClassIdx in range(len(self.classes_)):
+                actClass = self.classes_[actClassIdx]
+                nb_prot = nb_ppc[actClassIdx]  # nb_ppc: prototypes per class
+                if (self.protos_initialized[actClassIdx] == 0 and
+                        actClass in unique_labels(train_lab)):
+                    mean = np.mean(
+                        train_set[train_lab == actClass, :], 0)
+
+                    if self.prototypes_per_class == 1:
+                        # If only one prototype we init it to mean
+                        self.w_[pos:pos + nb_prot] = mean
+                    else:
+                        # else we add some random noise to distribute them
+                        self.w_[pos:pos + nb_prot] = mean + (
+                            random_state.rand(nb_prot, nb_features) * 2 - 1)
+
+                    if math.isnan(self.w_[pos, 0]):
+                        raise ValueError('Prototype on position {} for class\
+                                         {} is NaN.'
+                                         .format(pos, actClass))
+                    else:
+                        self.protos_initialized[actClassIdx] = 1
+
+                    self.c_w_[pos:pos + nb_prot] = actClass
+                pos += nb_prot
+        else:
+            x = validation.check_array(self.initial_prototypes)
+            self.w_ = x[:, :-1]
+            self.c_w_ = x[:, -1]
+            if self.w_.shape != (np.sum(nb_ppc), nb_features):
+                raise ValueError("the initial prototypes have wrong shape\n"
+                                 "found=(%d,%d)\n"
+                                 "expected=(%d,%d)" % (
+                                     self.w_.shape[0], self.w_.shape[1],
+                                     nb_ppc.sum(), nb_features))
+            if set(self.c_w_) != set(self.classes_):
+                raise ValueError(
+                    "prototype labels and test data classes do not match\n"
+                    "classes={}\n"
+                    "prototype labels={}\n".format(self.classes_, self.c_w_))
+        if self.initial_fit:
+            self.initial_fit = False
+
+        return train_set, train_lab
 
 
--- BA_thesis_code/BA_thesis_code/algorithms/RSLVQAdadelta.py
+++ BA_thesis_code/BA_thesis_code/algorithms/RSLVQAdadelta.py
@@ -33,24 +33,3 @@ class RSLVQAdadelta(RSLVQ):
 
                 # Attract/Distract prototype to/from data point
                 self.w_[j] += step
-
-    def _p(self, j, e, y=None, prototypes=None, **kwargs):
-         if prototypes is None:
-            prototypes = self.w_
-            if y is None:
-                fs = [self._costf(e, w, **kwargs) for w in prototypes]
-            else:
-                fs = [self._costf(e, prototypes[i], **kwargs) for i in
-                     range(prototypes.shape[0]) if
-                     self.c_w_[i] == y]
-
-            fs_max = np.max(fs)
-            s = sum([np.math.exp(f - fs_max) for f in fs])
-            o = np.math.exp(
-            self._costf(e, prototypes[j], **kwargs) - fs_max) / s
-            return o
-
-    def _costf(self, x, w, **kwargs):
-        d = (x - w)[np.newaxis].T
-        d = d.T.dot(d)
-        return -d / (2 * self.sigma)
